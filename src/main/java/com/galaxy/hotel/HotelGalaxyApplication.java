7bd0cd4e98f453b7eafd14013d98ade6627854bc
patelpraveen110 <patelpraveen110@gmail.com>
2024-07-24 23:27:55 +0530

    first commit

---
+++ src/main/java/com/galaxy/hotel/HotelGalaxyApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelGalaxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelGalaxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galaxy/hotel/HotelGalaxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelGalaxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
